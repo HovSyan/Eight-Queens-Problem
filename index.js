67df3ff43e81fa81bb6b5d20a565383d6779f1a8
Hovhannes <Hovhannes Sahakyan>
2019-04-14 11:42:20 +0400

    [replace] all tabs to spaces

--- index.js
+++ index.js
@@ -10,85 +10,85 @@ let solutionFound = false;
 let attemptsCount = 0;
 
 function getQueensRandomPlacement() {
-	let queens = [];
+  let queens = [];
 
-	for(let i = 0; i < QUEENS_COUNT; i++) {
-		let x = i;
-		let y = parseInt(8 * Math.random());
+  for(let i = 0; i < QUEENS_COUNT; i++) {
+    let x = i;
+    let y = parseInt(8 * Math.random());
 
-		queens.push({ x: x, y: y });
-	}
+    queens.push({ x: x, y: y });
+  }
 
-	return queens;
+  return queens;
 }
 
 function areStrikingEachOther(queens) {
-	chessBoard = [
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0],
-	];
+  chessBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
-	for(let i = 0; i < QUEENS_COUNT; i++) {
-		if(chessBoard[queens[i]['x']][queens[i]['y']] == 1) {
-			return true;
-		}
+  for(let i = 0; i < QUEENS_COUNT; i++) {
+    if(chessBoard[queens[i]['x']][queens[i]['y']] == 1) {
+      return true;
+    }
 
-		chessBoard[queens[i]['x']][queens[i]['y']] = 1;
-	}
+    chessBoard[queens[i]['x']][queens[i]['y']] = 1;
+  }
 
-	for(let  i = 0; i < chessBoard.length; i++) {
-		for(let  j = 0; j < chessBoard[i].length; j++) {
-			if(chessBoard[i][j] == 1) {
-				for(let k = j + 1; k < chessBoard[i].length; k++) {
-					if(chessBoard[i][k] == 1) {
-						return true;
-					}
-				}
+  for(let  i = 0; i < chessBoard.length; i++) {
+    for(let  j = 0; j < chessBoard[i].length; j++) {
+      if(chessBoard[i][j] == 1) {
+        for(let k = j + 1; k < chessBoard[i].length; k++) {
+          if(chessBoard[i][k] == 1) {
+            return true;
+          }
+        }
 
-				for(let k = i + 1; k < chessBoard.length; k++) {
-					if(chessBoard[k][j] == 1) {
-						return true;
-					}
-				}
-   
-				for(let k = i + 1, l = j - 1; k < chessBoard.length && l >= 0; (k++, l--)) {
-					if(chessBoard[k][l] == 1) {
-						return true;
-					}
-				}
+        for(let k = i + 1; k < chessBoard.length; k++) {
+          if(chessBoard[k][j] == 1) {
+            return true;
+          }
+        }
 
-				for(let k = i + 1, l = j + 1; k < chessBoard.length && l < chessBoard[k].length; (k++, l++)) {
-					if(chessBoard[k][l] == 1) {
-						return true;
-					}
-				}
-			}
-		}
-	}
+        for(let k = i + 1, l = j - 1; k < chessBoard.length && l >= 0; (k++, l--)) {
+          if(chessBoard[k][l] == 1) {
+            return true;
+          }
+        }
 
-	return false;
+        for(let k = i + 1, l = j + 1; k < chessBoard.length && l < chessBoard[k].length; (k++, l++)) {
+          if(chessBoard[k][l] == 1) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+
+  return false;
 }
 
 while(!solutionFound) {
-	let queens = getQueensRandomPlacement();
+  let queens = getQueensRandomPlacement();
 
-	if(!areStrikingEachOther(queens)) {
-		solutionFound = true;
-		console.log(chessBoard);
-		break;
-	}
+  if(!areStrikingEachOther(queens)) {
+    solutionFound = true;
+    console.log(chessBoard);
+    break;
+  }
 
-	attemptsCount++;
-	console.log(attemptsCount);
+  attemptsCount++;
+  console.log(attemptsCount);
 
-	if(attemptsCount == 200000) {
-		console.log('Try Again');
-		break;
-	}		
+  if(attemptsCount == 200000) {
+    console.log('Try Again');
+    break;
+  }
 }
